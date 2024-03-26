ac128dfc34d836666a9a67499b19c67754262c19
tejasnasre <tejasnasre120@gmail.com>
2024-03-26 22:46:41 +0530

    3d Rose

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import "./App.css";
+import { Canvas } from "@react-three/fiber";
+import { useGLTF, Stage, PresentationControls } from "@react-three/drei";
+
+function Model(props) {
+  const { scene } = useGLTF("/3dpea.glb");
+  return <primitive object={scene} {...props} />
+}
+
+function App() {
+  return (
+    <Canvas dpr={[1,2]} shadows camera={{ fov: 45 }} style={{"position": "absolute"}}>
+      <color attach="background" args={["#FFC0CB"]} />
+      <PresentationControls speed={1.5} global zoom={.8} polar={[-0.1, Math.PI / 4]}>
+        <Stage environment={"sunset"}>
+          <Model scale={0.01} />
+        </Stage>
+      </PresentationControls>
+    </Canvas>
+  );
+}
+
+export default App;
